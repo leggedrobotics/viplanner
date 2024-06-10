b19a86980ae33888d0a54cc25bb15a54c7ec6329
Pascal <roth.pascal@outlook.de>
2024-06-10 16:51:28 +0200

    remove wrong header

--- omniverse/extension/omni.viplanner/omni/viplanner/viplanner/mdp/commands/path_follower_command_generator.py
+++ omniverse/extension/omni.viplanner/omni/viplanner/viplanner/mdp/commands/path_follower_command_generator.py
@@ -4,11 +4,6 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-# Copyright (c) 2022-2024, The ORBIT Project Developers.
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
 """Sub-module containing command generators for the velocity-based locomotion task."""
 from __future__ import annotations
 
